76a17351d977ef5844c4c496602dec192aad89d1
Olumide Ekundayo <ekundayoolumide1@addosser.com>
2026-05-12 23:08:09 +0100

    made changes

--- src/app/page/simbiatuu/simbiatuu.component.ts
+++ src/app/page/simbiatuu/simbiatuu.component.ts
@@ -11,11 +11,13 @@ import {
   faShareAlt,
   faUsers,
 } from '@fortawesome/free-solid-svg-icons';
+import { HeaderComponent } from "../../components/header/header.component";
+// import { FooterComponent } from "../../components/footer/footer.component";
 
 @Component({
   selector: 'app-simbiatuu',
   standalone: true,
-  imports: [FaIconComponent],
+  imports: [FaIconComponent, HeaderComponent],
   templateUrl: './simbiatuu.component.html',
   styleUrl: './simbiatuu.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
